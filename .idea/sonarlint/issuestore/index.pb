f4a65a3593ef7d9dbdf8b2ee62b4b0380813e8ec
pauleramdf <paulomdf741@gmail.com>
2022-03-17 17:15:37 -0300

    anotação crossorigin

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+c
+3src/main/java/stock/model/StocksHistoricPrices.java,e/3/e3eaec1fc6dc2e4b19c76cc51a2c587b68766366
+[
++src/main/java/stock/dto/StockPricesDto.java,a/a/aa8e0c7f26a392f985aa5b5b4bc637c5802a8cfa
--- src/main/java/stock/controller/StocksRestController.java
+++ src/main/java/stock/controller/StocksRestController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = {"http://localhost:8080", "http://localhost:8082"})
 @RequiredArgsConstructor
 class StocksRestController {
 
